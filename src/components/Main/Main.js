b1cf9746a4a82993fbef4222b010e5934b35aa85
Anurag Sachdeva <anurag.sach28@gmail.com>
2019-06-13 13:24:51 +0530

    bootstrap integrated form to add client

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,7 @@ import ContestPage from '../ContestPage/ContestPage';
 import Dashboard from '../Dashboard/Dashboard';
 import QuestionPage from '../QuestionPage/QuestionPage';
 import './Main.css';
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 
 class Main extends Component {
     render() {
@@ -18,8 +18,52 @@ class Main extends Component {
                         exact />
                 </div>
                 <div className="mainAside">
-                    <h4>NEW CLIENT</h4>
-                    <Form>
+                    <div className="mainAside_header">
+                        <h5 className="new_client">NEW CLIENT</h5>
+                    </div>
+                    <div className="mainAside_body">
+                        <Form>
+                            <Form.Group as={Row} >
+                                <Form.Label column sm="2" className="clientDetail">
+                                Client name
+                                </Form.Label>
+                                <Col sm="4">
+                                <Form.Control type="text" placeholder="" className="field" />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} >
+                                <Form.Label column sm="2" className="clientDetail">
+                                Upload logo
+                                </Form.Label>
+                                <Col sm="4">
+                                <Form.Control type="password" placeholder=" " className="field" />
+                                </Col>
+                                <Col sm="2">
+                                    <Form.Label column sm="2" className="upload">
+                                        <Form.Control type="file" className="upload" />
+                                        Choose file
+                                    </Form.Label>
+                                
+                                    {/* <Button variant="secondary" size="sm" className="upload">Choose file</Button> */}
+                                </Col>
+                            </Form.Group>
+                            <br />  
+                            <Form.Group as={Row} >
+                                <Col sm="2">
+                                    <Button variant="secondary" size="sm" className={`cancel`}>CANCEL</Button>
+                                </Col>
+                                <Col sm="2">
+                                    <Button variant="secondary" size="sm" className={`create`}>CREATE</Button>
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                    </div>
+                    
+                    
+                </div>
+                
+                {/* <Form>
                         <Form.Group as={Row} controlId="formPlaintextEmail">
                             <Form.Label column sm="2">
                             Email
@@ -37,9 +81,7 @@ class Main extends Component {
                             <Form.Control type="password" placeholder="Password" />
                             </Col>
                         </Form.Group>
-                    </Form>
-                    
-                </div>
+                    </Form> */}
             </div>
             
         );
