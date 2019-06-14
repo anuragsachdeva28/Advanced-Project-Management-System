f03b05e254c27830c94bcac313c8fbed763e8452
Anurag Sachdeva <anurag.sach28@gmail.com>
2019-06-14 16:50:29 +0530

    add tasks page

---
+++ src/components/Main/Autocomplete.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Autocomplete extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Autocomplete;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,23 +5,26 @@ import Dashboard from '../Dashboard/Dashboard';
 import QuestionPage from '../QuestionPage/QuestionPage';
 import AddClient from '../Main/AddClient';
 import './Main.css';
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import CardList from './CardList';
-
+import { Form, Row, Button } from 'react-bootstrap';
+import Autocomplete from './Autocomplete';
 
 class Main extends Component {
     render() {
         return (
             <div className="outer">
-                <div className="main">
+            <div className="main">
+                
                     <Route path="/Clients" component={Dashboard}  />
                     
-                </div>
+                
+            </div>
                 
                     <Route path="/Clients/add" component={AddClient}  />
+            <div className="projAside">    
                 
-                <div className="projAside">
-
+                {/* <Col sm="12" className="column"> */}
                     <div className="projHeader">
                         <div className="projHeaderName">
                             <h5 className="projList">PROJECT</h5>
@@ -32,6 +35,9 @@ class Main extends Component {
                             </div>
                         </div>
                     </div>
+                    {/* </Col> */}
+
+                    <div className="projHeader_fake"></div>
 
                     <div className="cards">
                         <CardList />
@@ -40,9 +46,51 @@ class Main extends Component {
                         <CardList />
 
                     </div>
+            </div>      
                     
+            {/* </Col>
+            <Col sm="7"> */}
+                <div className="addTask">
+                    <div className="taskHeader">
+                        <button className="add_task" type="button"> <span>+</span> Add Task </button>
+                    </div>
+                        
+                        <Form className="basic">
+                            <Form.Group as={Row} >
+                                <Form.Label className="formLabel">
+                                PROJECT NAME
+                                </Form.Label>
+                                <Form.Control type="text" placeholder="Write here...." className="nameField" />   
+                            </Form.Group>
+
+                            <Form.Group as={Row} className="formGroup" >
+                                <Form.Label className="formLabel">
+                                PROJECT DESCRIPTION
+                                </Form.Label>
+                                <Form.Control as="textarea" placeholder="Write here...." className="desField" />  
+                            </Form.Group>
+
+                            <Form.Group as={Row} className="formGroup" >
+                                <Form.Label className="formLabel">
+                                Team
+                                </Form.Label>
+                                <br></br>
+                                <Form.Control type="text" placeholder="Add members" className="addMember" />
+                                <Autocomplete suggestions={["White", "Black", "Green", "Blue", "Yellow", "Red"]} />  
+                            </Form.Group>
+
+                            <br />  
+                            <Form.Group as={Row} >
+                                <Col sm="3">
+                                    <Button variant="secondary" size="sm" className={`cancel`}>CANCEL</Button>
+                                </Col>
+                                <Col sm="3">
+                                    <Button variant="secondary" size="sm" className={`create`}>CREATE</Button>
+                                </Col>
+                            </Form.Group>
+                        </Form>
                 </div>
-                
+            {/* </Col> */}
             </div>
             
         );
