049c57c6b8aa2ffb5d12802af490e34d489a4b93
Anurag <anurag.sach28@gmail.com>
2019-06-20 21:40:33 +0530

    responsive

--- src/components/Main/Employee.js
+++ src/components/Main/Employee.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Employee.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Employee extends Component {
   render() {
@@ -28,41 +28,48 @@ class Employee extends Component {
             elit. Eligendi, reprehenderit!
           </p>
           <br />
+
           <div className="client-tableHeader">
             <div className="num"></div>
             <div className="username light">User Name</div>
             <div className="phone">Phone No.</div>
             <div className="email">Email Address</div>
+            <div className="role">Role</div>
             {/*<div className="status">Status</div>*/}
             {/*<div className="arrow"></div>*/}
           </div>
+          <div className="tableContainer">
+            <div className="client-tableBody">
+              <div className="num">1</div>
+              <div className="username">Christel Woolfolk Quiroga </div>
+              <div className="phone">9876543210</div>
+              <div className="email">abcdefg@gmail.com</div>
+              <div className="role">Manager</div>
+            </div>
 
-          <div className="client-tableBody">
-            <div className="num">1</div>
-            <div className="username">Christel Woolfolk Quiroga </div>
-            <div className="phone">9876543210</div>
-            <div className="email">abcdefg@gmail.com</div>
-          </div>
+            <div className="client-tableBody">
+              <div className="num">2</div>
+              <div className="username">Galen Woolfolk </div>
+              <div className="phone">9876543210</div>
+              <div className="email">abcdefgsfsfsfsf@something.com</div>
+              <div className="role">Designer</div>
+            </div>
 
-          <div className="client-tableBody">
-            <div className="num">2</div>
-            <div className="username">Galen Woolfolk </div>
-            <div className="phone">9876543210</div>
-            <div className="email">abcdefgsfsfsfsf@something.com</div>
-          </div>
+            <div className="client-tableBody">
+              <div className="num">3</div>
+              <div className="username">Galen Woolfolk </div>
+              <div className="phone">9876543210</div>
+              <div className="email">abcdefg@gmail.com</div>
+              <div className="role">Designer</div>
+            </div>
 
-          <div className="client-tableBody">
-            <div className="num">3</div>
-            <div className="username">Galen Woolfolk </div>
-            <div className="phone">9876543210</div>
-            <div className="email">abcdefg@gmail.com</div>
-          </div>
-
-          <div className="client-tableBody">
-            <div className="num">4</div>
-            <div className="username">Galen Woolfolk </div>
-            <div className="phone">9876543210</div>
-            <div className="email">abcdefgsfsfsfsf@something.com</div>
+            <div className="client-tableBody">
+              <div className="num">4</div>
+              <div className="username">Galen Woolfolk </div>
+              <div className="phone">9876543210</div>
+              <div className="email">abcdefgsfsfsfsf@something.com</div>
+              <div className="role">Manager</div>
+            </div>
           </div>
         </div>
       </div>
